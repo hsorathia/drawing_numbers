9c027934917a87359ac1d9a5c26f4fb47e90c585
wilsonzhang6 <wilsonzhang6@gmail.com>
2019-12-08 10:47:40 -0800

    login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from load import *
 from flask import Flask, render_template, flash, redirect, url_for, request
 from flask import current_app as app
+from flask_login import current_user, login_user, logout_user, login_required
+from app.forms import LoginForm, RegisterForm
+from app.models import User
 from werkzeug.urls import url_parse
 import numpy as np
 from PIL import Image
@@ -37,10 +40,32 @@ def data():
     print(d)
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('landing'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        # look at first result first()
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        # return to page before user got asked to login
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('landing')
+
+        return redirect(next_page)
+    return render_template('login.html', title='Sign in', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html', title='Register', form=form)
 
 def parseImg(imageData):
     # parse canvas image bytes and save as result.png
     imageString = re.search(b'base64,(.*)', imageData).group(1)
     with open('result.png', 'wb') as result:
         result.write(base64.decodebytes(imageString))
-
